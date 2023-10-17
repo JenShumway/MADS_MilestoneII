72e32e1f0c6db34c614b3d2d0ff782582041c7b4
Justin <githubwork@linuxwebdevelopment.com>
2023-10-17 18:29:09 -0500

    modified World Bank utility functions to be more modular. And modified their use in PCA file

--- utility_functions/world_bank_utility_functions.py
+++ utility_functions/world_bank_utility_functions.py
@@ -1,16 +1,11 @@
-# mapping world bank codes to definitions
-import pandas as pd
-
-mapping_world_bank_to_names_and_definitions_df = pd.read_csv('../WorldBankDatasets/Cleaned/World_Bank_Indicator_Definition_Info.csv',
-                                                             delimiter='\t')
+# This file has functions that map World Bank codes to definitions and names
 
 # get the feature name of a given World Bank indicator
 # Why? World Bank codes can seem very cryptic. This will give a full name to the code. If you want a full definition,
 # see the function get_world_bank_indicator_definition_from_code below
-def get_world_bank_indicator_name_from_code(world_bank_code, mapping_df=mapping_world_bank_to_names_and_definitions_df):
+def get_world_bank_indicator_name_from_code(world_bank_code, mapping_df):
     # print(f"World Bank Code {world_bank_code}")
     indicator_name_series = mapping_df[mapping_df['indicator_code'] == world_bank_code]['indicator_name']
-
     try:
         indicator_name = indicator_name_series.values[0]
     except:
@@ -20,9 +15,8 @@ def get_world_bank_indicator_name_from_code(world_bank_code, mapping_df=mapping_
     return indicator_name
 
 # get the full definition of a World Bank indicator from the code
-def get_world_bank_indicator_definition_from_code(world_bank_code, mapping_df=mapping_world_bank_to_names_and_definitions_df):
+def get_world_bank_indicator_definition_from_code(world_bank_code, mapping_df):
     indicator_definition_series = mapping_df[mapping_df['indicator_code'] == world_bank_code]['long_definition']
-    # indicator_definition = ''
     try:
         indicator_definition = indicator_definition_series.values[0]
     except:
